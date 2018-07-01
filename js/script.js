74f94dc8e216da1dcd2003fae197bf851a29d2c6
marlinrenthoek <38738665+marlinrenthoek@users.noreply.github.com>
2018-07-01 16:54:49 +0300

    поведение формы обратной связи

---
+++ js/script.js
@@ -0,0 +1,24 @@
+var link = document.querySelector(".сontact-info__link");
+var popup = document.querySelector(".feedback-form");
+var close = document.querySelector(".feedback-form__close-button");
+var login = popup.querySelector("[name=name]");
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("show-feedback-form");
+  login.focus();
+}); 
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("show-feedback-form");
+});
+
+window.addEventListener("keydown", function(evt) {
+  if (evt.keyCode ===27) {
+    if (popup.classList.contains("show-feedback-form")) {
+      evt.preventDefault();
+      popup.classList.remove("show-feedback-form");
+    }
+  }
+});
