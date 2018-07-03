0b706e468084d1115169d2bbdb462d4784e39032
marlinrenthoek <38738665+marlinrenthoek@users.noreply.github.com>
2018-07-03 08:54:41 +0300

    поведение элементов по tab

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ var link = document.querySelector(".сontact-info__link");
 var popup = document.querySelector(".feedback-form");
 var close = document.querySelector(".feedback-form__close-button");
 var login = popup.querySelector("[name=name]");
+var display = document.querySelector(".display-dropdown");
+var dropdown = document.querySelector(".site-navigation__dropdown-list");
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
@@ -20,5 +22,12 @@ window.addEventListener("keydown", function(evt) {
       evt.preventDefault();
       popup.classList.remove("show-feedback-form");
     }
+  }
+});
+
+display.addEventListener("keydown", function(evt) {
+  if (evt.keyCode ===32) {
+    evt.preventDefault();
+    dropdown.classList.add("display-dropdown-on");
   }
 });
