0bdefdcac05a9383b839d0c1ed82fa7943a8255d
marlinrenthoek <38738665+marlinrenthoek@users.noreply.github.com>
2018-07-06 17:45:17 +0300

    исправила стили для одинакого отображения в разных браузерах

--- js/script.js
+++ js/script.js
@@ -2,16 +2,19 @@ var link = document.querySelector(".contact-info__link");
 var popup = document.querySelector(".feedback-form");
 var close = document.querySelector(".feedback-form__close-button");
 var login = popup.querySelector("[name=name]");
+var overlay = document.querySelector(".feedback-form-overlay");
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("show-feedback-form");
+  overlay.classList.add("feedback-form-overlay-show");
   login.focus();
 }); 
 
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("show-feedback-form");
+  overlay.classList.remove("feedback-form-overlay-show");
 });
 
 window.addEventListener("keydown", function(evt) {
@@ -19,6 +22,13 @@ window.addEventListener("keydown", function(evt) {
     if (popup.classList.contains("show-feedback-form")) {
       evt.preventDefault();
       popup.classList.remove("show-feedback-form");
+      overlay.classList.remove("feedback-form-overlay-show");
     }
   }
+});
+
+overlay.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("show-feedback-form");
+  overlay.classList.remove("feedback-form-overlay-show");
 });
